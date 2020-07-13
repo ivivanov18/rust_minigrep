3e4eadb19fbba35cb0df09d453720adf35b79fdc
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-07-14 00:47:31 +0300

    refactor Config to have associated new function to create a config instead of parse

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config: Config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents = fs::read_to_string(config.filename).expect("Error while reading file");
     println!("{:?}", contents);
@@ -15,8 +15,10 @@ struct Config {
     filename: String
 }
 
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {query, filename}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
 }
