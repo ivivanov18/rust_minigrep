07409b4f060cdb1235bbc66847ec08db830892f4
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-07-17 21:36:51 +0300

    - add unit test for case insensitive - add implementation for search_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    
+
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+
+pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()){
             results.push(line.trim());         
         }
     }
@@ -42,15 +53,32 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duck tape.";
+
         assert_eq!(
             vec!["safe, fast, productive."], 
             search(query, contents)
         ); 
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_insensitive(query, contents)
+        );
     }
 }
