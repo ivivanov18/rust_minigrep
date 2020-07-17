d44e462352c7a068922815b48aee47e95bc8eb42
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-07-18 02:07:24 +0300

    - add possibility to print errors on std error

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args).unwrap();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     if let Err(err) = minigrep::run(config) {
-        println!("Application error {}", err);
+        eprintln!("Application error {}", err);
         process::exit(1);
     }
 }
