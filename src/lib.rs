78f1f41c485a9ad675b27f324cf4a3aa4b5a1eef
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-07-18 01:42:51 +0300

    - add logic to check whether CASE_INSENSITVE env variable is set - add case_sensitive attribute to Config struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,37 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
+#[derive(Debug)]
 pub struct Config {
    pub query: String,
-   pub filename: String
+   pub filename: String,
+   pub case_sensitive: bool
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             panic!("not enough arguments");
         }
         
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {query, filename}
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {query, filename, case_sensitive})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents) {
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap();
     if let Err(err) = minigrep::run(config) {
         println!("Application error {}", err);
         process::exit(1);
